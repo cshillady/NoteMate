2c3f2ae0f956af673189e726d8cc3678702689e7
Connor Shillady <c.shillady24@gmail.com>
2022-06-20 19:58:08 -0400

    Note Mate App

---
+++ src/app/note-card/note-card.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, ViewChild, ElementRef, Renderer2, AfterViewInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-note-card',
+  templateUrl: './note-card.component.html',
+  styleUrls: ['./note-card.component.scss']
+})
+export class NoteCardComponent implements OnInit {
+
+  @Input('title') title!: string;
+  @Input('body') body!: string;
+  @Input('link') link!: any;
+
+  @Output('delete') deleteEvent: EventEmitter<void> = new EventEmitter<void>();
+
+  @ViewChild('truncator', { static: true }) truncator!: ElementRef<HTMLElement>;
+  @ViewChild('bodyText', { static: true }) bodyText!: ElementRef<HTMLElement>;
+
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit() {
+    //
+    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    let viewableHeight = parseInt(style.getPropertyValue("height"), 10);
+
+    if (this.bodyText.nativeElement.scrollHeight > viewableHeight) {
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    } else {
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+    }
+  }
+  onXButtonClick() {
+    this.deleteEvent.emit();
+  }
+}
